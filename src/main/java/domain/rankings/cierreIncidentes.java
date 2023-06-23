1a877b3beffb551a8f8af41d9a970f346722d8a0
Lucas Boldrini <lucasnboldrini@hotmail.com>
2023-06-23 14:48:41 -0300

    ahora hay clases en el diagrama de clases de los rankings

--- src/main/java/domain/rankings/cierreIncidentes.java
+++ src/main/java/domain/rankings/cierreIncidentes.java
@@ -7,10 +7,10 @@ import java.util.List;
 public class cierreIncidentes {
     public List<Entidad> rankingMayorPromedioCierre;
 
-    public int calcularPromedio(Incidente incidente){
+    public int calcularTiempo(Incidente incidente){
        //return incidente.getFechaCierre() - incidente.getFechaApertura()
                 //TODO: arreglar los tipos de esto
-
+        return 1;
     }
 
 }
