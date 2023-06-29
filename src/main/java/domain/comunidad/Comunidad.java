df0c28c9bce0d98b1748c990013cf908748c6f7b
Lucas Boldrini <lucasnboldrini@hotmail.com>
2023-06-29 16:30:11 -0300

    Avances en los rankings

--- src/main/java/domain/comunidad/Comunidad.java
+++ src/main/java/domain/comunidad/Comunidad.java
@@ -1,7 +1,12 @@
 package domain.comunidad;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
 
+@Getter
+@Setter
 public class Comunidad {
     private List<Miembro> miembros;
     private List<Incidente> incidentes;
--- src/main/java/domain/rankings/cierreIncidentes.java
+++ src/main/java/domain/rankings/cierreIncidentes.java
@@ -1,31 +1,64 @@
 package domain.rankings;
 
+import com.twilio.rest.api.v2010.account.incomingphonenumber.Local;
 import domain.comunidad.Comunidad;
 import domain.comunidad.Incidente;
 import domain.entidadesDeServicio.Entidad;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
+import java.text.DateFormat;
 
+@Getter
+@Setter
 public class cierreIncidentes {
-    public List<Comunidad> comunidades;
-    public List<List<Incidente>> incidentesSemanales;
-    public List<Entidad> rankingMayorPromedioCierre;
-    public LocalDate fechaInicioSemana;
+    private List<Comunidad> comunidades;
+    private List<List<Incidente>> incidentesSemanales;
+    private List<Entidad> rankingMayorPromedioCierre;
 
+    private LocalDate fechaInicioSemana;
+    private LocalDate fechaFinSemana;
     public void CambiarFechaInicioSemana(){
-        if(ChronoUnit.DAYS.between(fechaInicioSemana, LocalDate.now()) == 7){
-            fechaInicioSemana = LocalDate.now();
-        }
-    }
-    public boolean estaDentroDeLaSemana(Incidente incidente) {
-        return if(ChronoUnit.DAYS.between(fechaInicioSemana, incidente.getFechaCierre()) )
+        fechaInicioSemana = LocalDate.now();
     }
 
-    public int tiempo(Incidente incidente){
-        TiempoQueTardo = incidente.getFechaCierre() - incidente.getFechaApertura();
+    public void CambiarFechaFinSemana(){
+        fechaFinSemana = LocalDate.now();
     }
+
+    public void obtenerTodosLosIncidentes() {
+        for (Comunidad comunidad : comunidades) {
+              incidentesSemanales.add(comunidad.getIncidentes());
+            }
+        }
+    public void filtrarXSemana(){
+
+    }
+
+    public void filtrarXEntidad(){
+
+
+    }
+       public boolean estaDentroDeLaSemana(Incidente incidente) {
+        long valorDeSemana = ChronoUnit.DAYS.between(fechaInicioSemana, incidente.getFechaCierre());
+        return valorDeSemana <= 7;
+    }
+    public int tiempoqueTarda(Incidente incidente) {
+        long diasDiferencia = ChronoUnit.DAYS.between(incidente.getFechaApertura(), incidente.getFechaCierre());
+        int diasDiferenciaEntero = (int) diasDiferencia;
+        return diasDiferenciaEntero;
+    }
+
+    //public int obtenerFechaEntero() {
+       // DateFormat formatoEntero = new SimpleDateFormat("yyyyMMdd");
+       // String fechaString = formatoEntero.format();
+      //  return Integer.parseInt(fechaString);
+   // }
+
 
 
 }
