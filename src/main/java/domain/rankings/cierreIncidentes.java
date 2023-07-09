74c53e1104f1c75673fedaaae1a6dd3f3a0c7855
Lucas Boldrini <lucasnboldrini@hotmail.com>
2023-07-09 19:52:27 -0300

    las fechas de inicio y fin de semana se actualizan solas

--- src/main/java/domain/rankings/cierreIncidentes.java
+++ src/main/java/domain/rankings/cierreIncidentes.java
@@ -5,7 +5,9 @@ import domain.comunidad.Incidente;
 import domain.entidadesDeServicio.Entidad;
 import lombok.Getter;
 import lombok.Setter;
+import org.quartz.impl.calendar.WeeklyCalendar;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.chrono.ChronoLocalDateTime;
 import java.time.temporal.ChronoUnit;
@@ -17,10 +19,24 @@ import java.util.stream.Collectors;
 @Setter
 public class cierreIncidentes {
 
-    LocalDate fechaComienzoSemana; //se tiene q cambiar cada lunes
-    LocalDate fechaFinSemana; //se tiene q cambiar cada domingo
-    List<Comunidad> comunidades;
-    List<List<Incidente>> listaIncidentes;
+    public LocalDate fechaComienzoSemana; //se tiene q cambiar cada lunes
+    public LocalDate fechaFinSemana; //se tiene q cambiar cada domingo
+    private List<Comunidad> comunidades;
+    private List<List<Incidente>> listaIncidentes;
+
+    public void cambiarFechaInicioSemana(LocalDate fecha){
+        if(fecha.isAfter(fechaComienzoSemana) && fecha.isAfter(fechaFinSemana) && fecha.getDayOfWeek() == DayOfWeek.MONDAY){
+            fechaComienzoSemana = fecha;
+        }
+    }
+
+    public void cambiarFechaFinSemana(LocalDate fecha){
+        if(fecha.isAfter(fechaComienzoSemana) && fecha.isAfter(fechaFinSemana) && fecha.getDayOfWeek() == DayOfWeek.SUNDAY){
+            fechaFinSemana = fecha;
+        }
+    }
+
+    // ver como hacer para que lo de arriba no quede con codigo repetido
 
     public List<Incidente> obtenerIncidentesDeComunidades(List<Comunidad> comunidades, List<List<Incidente>> listaIncidentes){
         for(Comunidad comunidad : comunidades){
