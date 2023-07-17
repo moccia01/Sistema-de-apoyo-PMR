8615028dfc11ab36b67fc1738a31dcbe6b3a63f3
NahuGim <nahuelegimenez@hotmail.com>
2023-07-17 00:42:49 -0300

    InformeV1

--- src/main/java/domain/entidadesDeServicio/Entidad.java
+++ src/main/java/domain/entidadesDeServicio/Entidad.java
@@ -5,6 +5,9 @@ import lombok.Setter;
 
 import java.util.Collections;
 import java.util.List;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.IOException;
 
 @Getter
 @Setter
@@ -16,6 +19,15 @@ public class Entidad {
     public void agregarEstablecimientos(Establecimiento ... establecimientos){
         Collections.addAll(this.listaEstablecimientos, establecimientos);
     }
+    public String leerInforme(String Informe) {
+        try {
+            return Files.readString(Paths.get(Informe));
+        } catch (IOException e) {
+            System.out.println("Error al leer el Informe: " + e.getMessage());
+            return null;
+        }
+    }
+
 }
 
 
