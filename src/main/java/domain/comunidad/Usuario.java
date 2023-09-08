273b0670470da92ea3c271e53876e484834fba7f
NahuGim <nahuelegimenez@hotmail.com>
2023-09-08 11:35:32 -0300

    Version nueva y una tablita nueva

--- src/main/java/domain/comunidad/Usuario.java
+++ src/main/java/domain/comunidad/Usuario.java
@@ -5,16 +5,30 @@ import domain.validaciones.CredencialDeAcceso;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
+
 @Setter
 @Getter
 
+@Entity
+@Table(name = "usuario")
 public class Usuario {
+    @Id
+    @GeneratedValue
+    private int id;
+    @Column(name = "nombre")
     private String nombre;
+    @Column(name = "apellido")
     private String apellido;
+    @Transient
     private CredencialDeAcceso credencialDeAcceso;
+    @Transient
     private String mail;
+    @Transient
     private String telefono;
+    @Transient
     private Interes interes;
+    @Transient
     private Localizacion localizacion;
 
     public void setLocalizacion(String provincia, String departamento, String direccion){
