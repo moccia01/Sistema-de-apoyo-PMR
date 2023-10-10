d2b5d23bf073587098930bb7b54125ffd0864520
moccia01 <federico21433@hotmail.com>
2023-10-10 19:26:51 -0300

    Avances con integracion API

---
+++ src/main/java/domain/models/entities/services/gradoCalculadorEquipo5/entities/ComunidadApi.java
@@ -0,0 +1,13 @@
+package domain.models.entities.services.gradoCalculadorEquipo5.entities;
+
+import domain.models.entities.comunidad.Comunidad;
+import domain.models.entities.comunidad.Incidente;
+import domain.models.entities.comunidad.Usuario;
+
+import java.util.List;
+
+public class ComunidadApi {
+    Comunidad comunidad;
+    List<Incidente> incidentes;
+
+}
---
+++ src/main/java/domain/models/entities/services/gradoCalculadorEquipo5/entities/UsuarioApi.java
@@ -0,0 +1,11 @@
+package domain.models.entities.services.gradoCalculadorEquipo5.entities;
+
+import domain.models.entities.comunidad.Incidente;
+import domain.models.entities.comunidad.Usuario;
+
+import java.util.List;
+
+public class UsuarioApi {
+    Usuario usuariol;
+    List<Incidente> incidentes;
+}
